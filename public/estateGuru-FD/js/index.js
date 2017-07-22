40149fba511593af4c92b4b08dd4d439da09abd8
Max Nikulin <perseusxii@gmail.com>
2017-07-22 18:52:39 +0700

    правки для квратиры

--- public/estateGuru-FD/js/index.js
+++ public/estateGuru-FD/js/index.js
@@ -1,9 +1,9 @@
 import './components/slider-promo';
 import './components/slider-logos';
-import './components/slider-nav';
+// import './components/slider-nav';
 import './components/slider-layouts';
 import './components/visual';
-import './components/gallery-polygons';
+// import './components/gallery-polygons';
 import './components/gallery-photos';
 import './components/gallery-layouts';
 import './components/popup';
