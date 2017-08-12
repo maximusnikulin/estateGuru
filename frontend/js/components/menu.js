e521bc27149bdf725fa3bdef72cb2036c8bb309d
Max Nikulin <perseusxii@gmail.com>
2017-08-12 18:38:32 +0700

    добавил контекстное меню

--- frontend/js/components/menu.js
+++ frontend/js/components/menu.js
@@ -4,8 +4,10 @@ var overlay = document.querySelector('.menu__overlay');
 var container = document.querySelector('.menu__container'); 
 
 bar.addEventListener('click',function(){
-    menu.classList.add('open')
+    menu.classList.add('open');
+    document.body.classList.add('frozen');
 });
 overlay.addEventListener('click',function(){
-    menu.classList.remove('open')
+    menu.classList.remove('open');
+    document.body.classList.add('frozen');
 });
