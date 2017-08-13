6215305a9f471acdc1add8052985d7ab80555b55
Max Nikulin <perseusxii@gmail.com>
2017-08-13 17:55:39 +0700

    слайдер застройщиков

--- frontend/js/components/slider-logos.js
+++ frontend/js/components/slider-logos.js
@@ -1,4 +1,5 @@
 import slick from 'slick-carousel';
+
 const SETTINGS = {
     infinite: true,
     slidesToShow: 4,
@@ -26,5 +27,21 @@ const SETTINGS = {
 
 $(window).on('load', function () {
     var $slider = $('.js-slider-logos');
+
+
     $slider.slick(SETTINGS)
+
+
+    var tabs = $(".main__logos");
+    var links = tabs.find('.link');
+    $('.link').on('click', function(e){
+        var id = $(this).data("id");
+        $('.link').removeClass('active');
+        $(this).addClass('active');
+        $('.slider-logos').css('display','none');
+
+        $('.slider-logos[data-id=' + id  +']').css('display','block');
+        $('.slider-logos[data-id=' + id  +']').slick('slickGoTo',0, true)
+    })
+
 });
